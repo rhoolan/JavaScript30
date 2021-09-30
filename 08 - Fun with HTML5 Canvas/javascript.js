c2380dcec03425576ffd3b36f934393aba5eeaac
Rob <rhoolan@gmail.com>
2021-09-30 13:12:00 +0100

    Delete unused code

--- 08 - Fun with HTML5 Canvas/javascript.js
+++ 08 - Fun with HTML5 Canvas/javascript.js
@@ -37,7 +37,3 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
-
-
-
-// document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
